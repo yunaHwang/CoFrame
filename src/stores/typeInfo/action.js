20ff5097300a4cce5badb1c33ceef43900ea5f80
yunaHwang <rdking9812@gmail.com>
2025-06-10 11:08:33 -0500

    Change move gripper to open gripper

--- src/stores/typeInfo/action.js
+++ src/stores/typeInfo/action.js
@@ -37,194 +37,194 @@ const basicActionData = {
   ...baseTypeData
 };
 
-const delayFeatures = {
-  name: "Delay",
-  description: "A pause by the [Robot](robotAgentType) by a specified amount of time.",
-  properties: {
-    description: { default: "Delay action for a specified amount of time" },
-    duration: {
-      name: "Duration",
-      type: SIMPLE_PROPERTY_TYPES.NUMBER,
-      default: 1,
-      min: 0,
-      max: 3600,
-      step: 10,
-      visualScaling: 1,
-      visualPrecision: 1,
-      units: "sec",
-    },
-    compileFn: { default: COMPILE_FUNCTIONS.DELAY },
-    updateFields: { default: ["duration"] },
-  },
-};
+// const delayFeatures = {
+//   name: "Delay",
+//   description: "A pause by the [Robot](robotAgentType) by a specified amount of time.",
+//   properties: {
+//     description: { default: "Delay action for a specified amount of time" },
+//     duration: {
+//       name: "Duration",
+//       type: SIMPLE_PROPERTY_TYPES.NUMBER,
+//       default: 1,
+//       min: 0,
+//       max: 3600,
+//       step: 10,
+//       visualScaling: 1,
+//       visualPrecision: 1,
+//       units: "sec",
+//     },
+//     compileFn: { default: COMPILE_FUNCTIONS.DELAY },
+//     updateFields: { default: ["duration"] },
+//   },
+// };
 
-const breakpointFeatures = {
-  name: "Breakpoint",
-  description: "A terminator that prematurely ends the compilation of the [Program](programType).",
-  properties: {
-    description: { default: "Stop computation and processing here" },
-    compileFn: { default: COMPILE_FUNCTIONS.BREAK },
-    updateFields: { default: [] },
-  },
-};
+// const breakpointFeatures = {
+//   name: "Breakpoint",
+//   description: "A terminator that prematurely ends the compilation of the [Program](programType).",
+//   properties: {
+//     description: { default: "Stop computation and processing here" },
+//     compileFn: { default: COMPILE_FUNCTIONS.BREAK },
+//     updateFields: { default: [] },
+//   },
+// };
 
 const gripperFeatures = {
-  name: "Move Gripper",
+  name: "Open Gripper",
   description: "An action by the [Robot](robotAgentType) that adjusts the distance between the two fingers of the gripper. If interacting with a [Thing](thingType) or [Tool](toolType), it should be specified in the action.",
   properties: {
-    description: { default: "Adjust the gripper position" },
+    description: { default: "Fully open the gripper" },
     thing: {
-      name: "Thing",
-      accepts: ["thingType", "toolType"],
+      name: "Object",
+      accepts: ["thingType"],
       default: null,
       isList: false,
       nullValid: true,
     },
-    positionStart: {
-      name: "Start Position",
-      type: SIMPLE_PROPERTY_TYPES.NUMBER,
-      default: 50,
-      min: 0,
-      max: 85,
-    },
-    positionEnd: {
-      name: "End Position",
-      type: SIMPLE_PROPERTY_TYPES.NUMBER,
-      default: 50,
-      min: 0,
-      max: 85,
-    },
-    speed: {
-      name: "Speed",
-      type: SIMPLE_PROPERTY_TYPES.NUMBER,
-      default: 20,
-      min: 1,
-      max: 50,
-      step: 1,
-      visualScaling: 1,
-      visualPrecision: 0,
-      units: "mm/s",
-    },
+    // positionStart: {
+    //   name: "Start Position",
+    //   type: SIMPLE_PROPERTY_TYPES.NUMBER,
+    //   default: 50,
+    //   min: 0,
+    //   max: 85,
+    // },
+    // positionEnd: {
+    //   name: "End Position",
+    //   type: SIMPLE_PROPERTY_TYPES.NUMBER,
+    //   default: 50,
+    //   min: 0,
+    //   max: 85,
+    // },
+    // speed: {
+    //   name: "Speed",
+    //   type: SIMPLE_PROPERTY_TYPES.NUMBER,
+    //   default: 20,
+    //   min: 1,
+    //   max: 50,
+    //   step: 1,
+    //   visualScaling: 1,
+    //   visualPrecision: 0,
+    //   units: "mm/s",
+    // },
     compileFn: { default: COMPILE_FUNCTIONS.GRIPPER_MOTION },
     updateFields: {
-      default: ["thing", "positionStart", "positionEnd", "speed"],
+      default: ["thing"],
     },
   },
 };
 
-const machineInitFeatures = {
-  name: "Machine Initialize",
-  description: "An action that initializes the [Machine](machineType) for usage. This need only be done once per execution of the [Program](programType).",
-  properties: {
-    description: { default: "Initialize a machine for use" },
-    machine: {
-      name: "Machine",
-      accepts: ["machineType"],
-      default: null,
-      isList: false,
-    },
-    compileFn: { default: COMPILE_FUNCTIONS.MACHINE },
-    updateFields: { default: ["machine"] },
-  },
-};
+// const machineInitFeatures = {
+//   name: "Machine Initialize",
+//   description: "An action that initializes the [Machine](machineType) for usage. This need only be done once per execution of the [Program](programType).",
+//   properties: {
+//     description: { default: "Initialize a machine for use" },
+//     machine: {
+//       name: "Machine",
+//       accepts: ["machineType"],
+//       default: null,
+//       isList: false,
+//     },
+//     compileFn: { default: COMPILE_FUNCTIONS.MACHINE },
+//     updateFields: { default: ["machine"] },
+//   },
+// };
 
-const processStartFeatures = {
-  name: "Process Start",
-  description: "An action that begins a process [Process](processType). If the process needs a [Machine](machineType) or [Tool](toolType), this must also be provided in the 'gizmo' field.",
-  properties: {
-    description: { default: "Begin a machine process" },
-    process: {
-      name: "Process",
-      accepts: ["processType"],
-      default: null,
-      isList: false,
-    },
-    gizmo: {
-      name: "Gizmo",
-      accepts: ["machineType","toolType"],
-      default: null,
-      isList: false,
-      nullValid: true,
-    },
-    compileFn: { default: COMPILE_FUNCTIONS.PROCESS },
-    updateFields: { default: ["process", "gizmo"] },
-  },
-};
+// const processStartFeatures = {
+//   name: "Process Start",
+//   description: "An action that begins a process [Process](processType). If the process needs a [Machine](machineType) or [Tool](toolType), this must also be provided in the 'gizmo' field.",
+//   properties: {
+//     description: { default: "Begin a machine process" },
+//     process: {
+//       name: "Process",
+//       accepts: ["processType"],
+//       default: null,
+//       isList: false,
+//     },
+//     gizmo: {
+//       name: "Gizmo",
+//       accepts: ["machineType","toolType"],
+//       default: null,
+//       isList: false,
+//       nullValid: true,
+//     },
+//     compileFn: { default: COMPILE_FUNCTIONS.PROCESS },
+//     updateFields: { default: ["process", "gizmo"] },
+//   },
+// };
 
-const processWaitFeatures = {
-  name: "Process Wait",
-  description: "An action that has the [Robot](robotAgentType) wait until the completion of the process [Process](processType). If the process needs a [Machine](machineType) or [Tool](toolType), this must also be provided in the 'gizmo' field.",
-  properties: {
-    description: {
-      default:
-        "Fill any remaining time while process is running by making the robot wait",
-    },
-    process: {
-      name: "Process",
-      accepts: ["processType"],
-      default: null,
-      isList: false,
-    },
-    gizmo: {
-      name: "Gizmo",
-      accepts: ["machineType","toolType"],
-      default: null,
-      isList: false,
-      nullValid: true,
-    },
-    compileFn: { default: COMPILE_FUNCTIONS.PROCESS },
-    updateFields: { default: ["process", "gizmo"] },
-  },
-};
+// const processWaitFeatures = {
+//   name: "Process Wait",
+//   description: "An action that has the [Robot](robotAgentType) wait until the completion of the process [Process](processType). If the process needs a [Machine](machineType) or [Tool](toolType), this must also be provided in the 'gizmo' field.",
+//   properties: {
+//     description: {
+//       default:
+//         "Fill any remaining time while process is running by making the robot wait",
+//     },
+//     process: {
+//       name: "Process",
+//       accepts: ["processType"],
+//       default: null,
+//       isList: false,
+//     },
+//     gizmo: {
+//       name: "Gizmo",
+//       accepts: ["machineType","toolType"],
+//       default: null,
+//       isList: false,
+//       nullValid: true,
+//     },
+//     compileFn: { default: COMPILE_FUNCTIONS.PROCESS },
+//     updateFields: { default: ["process", "gizmo"] },
+//   },
+// };
 
-const moveTrajectoryFeatures = {
-  name: "Move Trajectory",
-  description: "An action by the [Robot](robotAgentType) that moves its gripper along a specified [Trajectory](trajectoryType). Motion types include 'IK', which attempts to move the gripper in a straight line from one location to another, while 'Joint' interpolates the joints. Velocity adjusts the speed of the motion.",
-  properties: {
-    description: {
-      default: "Move Robot according to a trajectory and motion type",
-    },
-    trajectory: {
-      name: "Trajectory",
-      accepts: ["trajectoryType"],
-      default: null,
-      isList: false,
-    },
-    duration: {
-      // mm/ms or m/s
-      name: "Duration",
-      type: SIMPLE_PROPERTY_TYPES.NUMBER,
-      default: 1,
-      min: 0.01,
-      max: 60,
-      step: 0.01,
-      visualScaling: 1,
-      visualPrecision: 2,
-      units: "s",
-    },
-    motionType: {
-      name: "Motion Type",
-      type: SIMPLE_PROPERTY_TYPES.OPTIONS,
-      options: [{value: "IK", label: "IK"}, {value: "Joint", label:'Joint'}],
-      default: "IK",
-    },
-    compileFn: { default: COMPILE_FUNCTIONS.ROBOT_MOTION },
-    updateFields: { default: ["trajectory", "duration", "motionType"] },
-  },
-};
+// const moveTrajectoryFeatures = {
+//   name: "Move Trajectory",
+//   description: "An action by the [Robot](robotAgentType) that moves its gripper along a specified [Trajectory](trajectoryType). Motion types include 'IK', which attempts to move the gripper in a straight line from one location to another, while 'Joint' interpolates the joints. Velocity adjusts the speed of the motion.",
+//   properties: {
+//     description: {
+//       default: "Move Robot according to a trajectory and motion type",
+//     },
+//     trajectory: {
+//       name: "Trajectory",
+//       accepts: ["trajectoryType"],
+//       default: null,
+//       isList: false,
+//     },
+//     duration: {
+//       // mm/ms or m/s
+//       name: "Duration",
+//       type: SIMPLE_PROPERTY_TYPES.NUMBER,
+//       default: 1,
+//       min: 0.01,
+//       max: 60,
+//       step: 0.01,
+//       visualScaling: 1,
+//       visualPrecision: 2,
+//       units: "s",
+//     },
+//     motionType: {
+//       name: "Motion Type",
+//       type: SIMPLE_PROPERTY_TYPES.OPTIONS,
+//       options: [{value: "IK", label: "IK"}, {value: "Joint", label:'Joint'}],
+//       default: "IK",
+//     },
+//     compileFn: { default: COMPILE_FUNCTIONS.ROBOT_MOTION },
+//     updateFields: { default: ["trajectory", "duration", "motionType"] },
+//   },
+// };
 
 const actionTypes = {
-  delayType: merge(delayFeatures, basicActionData),
+  //delayType: merge(delayFeatures, basicActionData),
   moveGripperType: merge(gripperFeatures, basicActionData),
-  machineInitType: merge(machineInitFeatures, basicActionData),
-  processStartType: merge(processStartFeatures, basicActionData),
+  //machineInitType: merge(machineInitFeatures, basicActionData),
+  //processStartType: merge(processStartFeatures, basicActionData),
   // processStopType: merge(processStopFeatures, basicActionData),
-  processWaitType: merge(processWaitFeatures, basicActionData),
-  moveTrajectoryType: merge(moveTrajectoryFeatures, basicActionData),
+  //processWaitType: merge(processWaitFeatures, basicActionData),
+  //moveTrajectoryType: merge(moveTrajectoryFeatures, basicActionData),
   // moveUnplannedType: merge(moveUnplannedFeatures,basicActionData),
-  breakpointType: merge(breakpointFeatures, basicActionData, {
-  instanceBlock: { color: "#3a5e40" },
-  }),
+  // breakpointType: merge(breakpointFeatures, basicActionData, {
+  // instanceBlock: { color: "#3a5e40" },
+  // }),
 };
 
 console.log(actionTypes);
