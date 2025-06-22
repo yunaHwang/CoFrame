34c465264b5baaeedbf39a7ba6f8f4dc526c09f6
yunaHwang <rdking9812@gmail.com>
2025-06-22 16:51:47 -0500

    Width is larger than height; this is the point if you every want to revert

--- src/components/Body/RobotWorld.jsx
+++ src/components/Body/RobotWorld.jsx
@@ -12,21 +12,18 @@ const RobotWorld = () => {
   const pageStyle = {
     minHeight: '40vh',
     width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#333333',
   };
 
   const gridStyle = {
     display: 'grid',
-    gridTemplateColumns: `repeat(10, ${CELL_SIZE}px)`,
+    width: '100%',
+    maxWidth: '100%',
+    gridTemplateColumns: 'repeat(10, 10%)',
     gridTemplateRows: `repeat(8, ${CELL_SIZE}px)`,
   };
 
   const cellStyle = {
-    width: CELL_SIZE,
-    height: CELL_SIZE,
     backgroundColor: '#ffffff',
     border: '1px solid #bbbbbb',
     boxSizing: 'border-box',
@@ -36,7 +33,7 @@ const RobotWorld = () => {
     <div style={pageStyle}>
       <div style={gridStyle}>
         {Array.from({ length: 80 }).map((_, idx) => (
-          <div key={idx} style={cellStyle} />
+          <div key={idx} style={{ ...cellStyle, height: CELL_SIZE }} />
         ))}
       </div>
     </div>
