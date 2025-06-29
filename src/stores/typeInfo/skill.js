e4360be1ea5dd4e736809ed8ee7f4bcaf97314c0
yunaHwang <rdking9812@gmail.com>
2025-06-29 13:03:20 -0500

    Expand list of actions that is accepted by clusterType (skillType)

--- src/stores/typeInfo/skill.js
+++ src/stores/typeInfo/skill.js
@@ -95,7 +95,7 @@ const emptySkillFeatures = {
     description: { default: "some string" },
     children: {
       name: "Sets of Actions",
-      accepts: ["moveGripperType", "closeGripperType", "stopStretchType", "saveLogStretchType",
+      accepts: ["moveGripperType", "closeGripperType", "lookForType", "handObjToType", "stopStretchType", "saveLogStretchType",
         "sayType", "moveForwardType", "rotateType", "slowerStretchType", "fasterStretchType", "toLocationType"
       ],
       default: [],
@@ -116,7 +116,7 @@ const emptyConcurrentFeatures = {
     description: { default: "some string" },
     children: {
       name: "Sets of Actions",
-      accepts: ["moveGripperType", "closeGripperType", "stopStretchType", "saveLogStretchType",
+      accepts: ["moveGripperType", "closeGripperType", "lookForType", "handObjToType", "stopStretchType", "saveLogStretchType",
         "sayType", "moveForwardType", "rotateType", "slowerStretchType", "fasterStretchType"
       ],
       default: [],
