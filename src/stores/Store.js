bb5c85e17b1dea78ec17e91ba758c05e03ac842e
yunaHwang <rdking9812@gmail.com>
2025-08-13 12:41:55 -0500

    Sensor testing complete (with transitions writing cleanly)

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -53,7 +53,7 @@ const store = (set, get) => ({
   fallbackMode: false,
   setFallbackMode : (value) => set({fallbackMode: value}),
 
-  globalBatteryLevel: 30,
+  globalBatteryLevel: 100,
   setGlobalBatteryLevel: (val) => set({ globalBatteryLevel: val }),
 
   globalDistanceTravel: 0,
@@ -63,7 +63,7 @@ const store = (set, get) => ({
   batteryWarning: true,
   setBatteryWarning: (value) => set({ batteryWarning: value }),
 
-  batteryLevel: 30,
+  batteryLevel: 100,
   setbatteryLevel: (value) => set({ batteryLevel: value }),
   
   //battery warnings for error popping
