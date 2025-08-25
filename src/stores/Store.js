53987c699a4f8190f47e61afde2c550ec2252d00
yunaHwang <rdking9812@gmail.com>
2025-08-25 14:21:13 -0500

    Battery warnings for concurrent actions working + might need some fix in transition_concur though

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -53,7 +53,7 @@ const store = (set, get) => ({
   fallbackMode: false,
   setFallbackMode : (value) => set({fallbackMode: value}),
 
-  globalBatteryLevel: 100, // change when testing
+  globalBatteryLevel: 30, // change when testing
   setGlobalBatteryLevel: (val) => set({ globalBatteryLevel: val }),
 
   globalDistanceTravel: 0, // change when testing
@@ -63,7 +63,7 @@ const store = (set, get) => ({
   batteryWarning: true,
   setBatteryWarning: (value) => set({ batteryWarning: value }),
 
-  batteryLevel: 100,
+  batteryLevel: 30,
   setbatteryLevel: (value) => set({ batteryLevel: value }),
   
   //battery warnings for error popping
